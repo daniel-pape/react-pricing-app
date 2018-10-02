179dc2b1dfcf2ae211862e4bf2d6129d9a442222
dp <daniel.pape@codecentric.de>
2018-10-02 17:26:24 +0200

    Cleansing

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,37 +7,39 @@ class Main extends Component {
     constructor(props) {
         super(props)
 
-        let id = 0;
-        const createData = function (name, price, quantity, unitPrice) {
-            id += 1;
-            return { id, name, price, quantity, unitPrice };
+        const createData = function (id, name, price, quantity, unitPrice) {
+            return {id, name, price, quantity, unitPrice};
         }
 
         this.createData = createData;
 
-        const rows = [
-            createData('Frozen yoghurt', 159, 6.0, 24),
-            createData('Ice cream sandwich', 237, 9.0, 37),
-            createData('Eclair', 262, 16.0, 24),
-            createData('Cupcake', 305, 3.7, 67),
-            createData('Gingerbread', 356, 16.0, 49),
-        ];
-
         this.state = {
-            rows: rows
+            rows: []
         }
 
         this.addProductHandler = this.addProductHandler.bind(this);
     }
 
-    addProductHandler = () => {
-        console.log("Product added")
+    addProductHandler = (data) => {
+        console.log("Product added", data)
+
+        const computeUnitPrice = function(price, quantity) {
+            return (price / quantity).toPrecision(2)
+        }
+
+
+        let newData = this.createData(
+            this.state.rows.length + 1,
+            data.productName,
+            data.price,
+            data.quantity,
+            computeUnitPrice(data.price, data.quantity),
+        );
+
 
         let newState = Object.assign({}, this.state);
-        let newData = this.createData('Gingerbread', 356, 16.0, 49);
         // TODO: Why does .push(newData) not work?
         newState["rows"] = this.state.rows.concat(newData);
-
         this.setState(newState);
 
     }
--- src/Components/ProductInput.js
+++ src/Components/ProductInput.js
@@ -23,52 +23,68 @@ class Inputs extends Component {
     };
 
     constructor(props) {
-        super(props)
+        super(props);
+
+        this.defaultState = {
+            productName: "",
+            price: "",
+            quantity: "",
+        }
+
+        this.state = this.defaultState
     }
 
     render() {
         return (
             <div className={this.props.classes.container}>
-
                 <Input
                     placeholder="Product Name"
+                    value={this.state.productName}
                     className={this.props.classes.input}
                     inputProps={{
                         'aria-label': 'Description',
                     }}
                     onChange={
                         (e) => {
-                            console.log("Product Name changes", e)
+                            this.setState({productName: e.target.value});
                         }
                     }
                 />
                 <Input
                     placeholder="Price"
+                    value={this.state.price}
                     className={this.props.classes.input}
                     inputProps={{
                         'aria-label': 'Description',
                     }}
+                    onChange={
+                        (e) => {
+                            this.setState({price: e.target.value});
+                        }
+                    }
                 />
                 <Input
                     placeholder="Quantity"
+                    value={this.state.quantity}
                     className={this.props.classes.input}
                     inputProps={{
                         'aria-label': 'Description',
                     }}
+                    onChange={
+                        (e) => {
+                            this.setState({quantity: e.target.value});
+                        }
+                    }
                 />
                 <Button
                     type="submit"
                     variant="contained"
                     color="primary"
-                    onClick={e => {
-                        console.log("Click", e);
-                        this.props.handler()
+                    onClick={() => {
+                        this.props.handler(this.state);
+                        this.setState(this.defaultState);
                     }}
-                >
-                    Add Product
-                </Button>
-
-
+                >Add Product</Button>
             </div>
         );
     }
